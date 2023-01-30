db75fed1f5ec8abe681b6521e9e68d83fd4f3cea
RoanLeCoder <>
2023-01-30 14:58:00 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+ 
+  
+  rect(width/14, height/6, 3*width/22, 3*height/4);  
+rect(width/3, height/6, 3*width/22, 3*height/4);  
+rect(width/1.5, height/6, 3*width/22, 3*height/4);
+rect(width/5, height/2.5, 125)  
+ rect(width/2, height/1, 3*width/4, 3*height/6)  
+  
+}
